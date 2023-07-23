ad72446cb02c678dd2f4b9530b4f625008df2bfe
Michael lima <michael.lima.dev@gmail.com>
2023-07-23 09:58:19 -0300

    dataset

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ const controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent, evento.target.parentNode);
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
     });
 });
 
 function manipulaDados(operation, controle){
-    const piece = controle.querySelector(".controle-contador");
+    const piece = controle.querySelector("[data-contador]");
 
     if(operation === "-"){
         piece.value = parseInt(piece.value) - 1;
